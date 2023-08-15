0dd2c0f4fa92ecbfc8b2b503bffb058b6f6e8786
Lucas Emanoel <102261954+lucasetdasilva@users.noreply.github.com>
2023-08-15 20:17:56 -0300

    Add files via upload

---
+++ springbootlab04/src/main/java/br/gov/sp/fatec/springbootlab04/Springbootlab04Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootlab04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlab04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlab04Application.class, args);
+	}
+
+}
---
+++ springbootlab04/src/test/java/br/gov/sp/fatec/springbootlab04/Springbootlab04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootlab04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootlab04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
